26a65b1a4fa84e493a34f4715f1a0dbb53fd6f42
Afonsoreidomarketing UwU <0000111975267xsp@al.educacao.sp.gov.br>
2024-11-26 15:41:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {    const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-    avanca.forEach(button => {
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        button.addEventListener('click', function () {
-
-            const atual = document.querySelector('.passo.ativo');
-
-            if (!atual) {
-                console.error('Nenhum passo ativo encontrado.');
-                return;
-            }
-
-            
-            const proximoPassoId = 'passo-' + this.getAttribute('data-proximo');
-
-            
-            const proximoPasso = document.getElementById(proximoPassoId);
-
-           
-            if (!proximoPasso) {
-                console.error(`Passo com ID "${proximoPassoId}" não encontrado.`);
-                return;
-            }
-
-
-            atual.classList.remove('ativo');
-
-            
-            proximoPasso.classList.add('ativo');
-        });
-    });
-});
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
